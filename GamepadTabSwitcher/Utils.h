270120b3ad6f3d4f1b4f7721c359f96b8cc742c7
Tudor3510 <tudorcioc@gmail.com>
2023-07-01 22:51:23 +0300

    Separated utils.h into utils.h and utils.cpp

--- GamepadTabSwitcher/Utils.h
+++ GamepadTabSwitcher/Utils.h
@@ -3,166 +3,27 @@
 
 #include <vector>
 #include <windows.h>
-#include <iostream>
-#include <fstream>
-#include <Psapi.h>
-#include <ShlObj.h>
 #include <string>
 #include "CXBOXController.h"
-#include <Xinput.h>
-#include <algorithm>
 
 struct WindowData {
     std::vector<HWND> windowsHandles;
     std::vector<std::string> reqProcess;
 };
 
-std::vector<std::string> ReadReqProcessFile(const std::string& filePath) {
-    std::vector<std::string> reqProcess;
-
-    std::ifstream fileIn(filePath);    
-    if (!fileIn.is_open()) {
-        std::cout << "Failed to open the file." << std::endl;
-        return reqProcess;
-    }
-
-    std::string line;
-    while (std::getline(fileIn, line)) {
-        reqProcess.push_back(line);
-    }
-
-    fileIn.close();  // Close the file
-
-    return reqProcess;
-}
-
-std::string GetProcessNameFromHWND(HWND hwnd) {
-    DWORD processId;
-    GetWindowThreadProcessId(hwnd, &processId);
-
-    HANDLE processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
-    if (processHandle) {
-        char processName[MAX_PATH];
-        if (GetModuleFileNameExA(processHandle, NULL, processName, MAX_PATH)) {
-            // Extract only the file name from the full path
-            std::string fullPath(processName);
-            std::size_t lastSlashPos = fullPath.find_last_of("\\/");
-            std::string processFileName = (lastSlashPos != std::string::npos) ? fullPath.substr(lastSlashPos + 1) : fullPath;
-
-            CloseHandle(processHandle);
-            return processFileName;
-        }
-        CloseHandle(processHandle);
-    }
-
-    return "";
-}
-
-std::string ConvertWideCharToMultiByte(const wchar_t* wideCharString) {
-    int length = WideCharToMultiByte(CP_UTF8, 0, wideCharString, -1, NULL, 0, NULL, NULL);
-    if (length == 0) {
-        return "";
-    }
-
-    std::string multiByteString(length, '\0');
-    WideCharToMultiByte(CP_UTF8, 0, wideCharString, -1, &multiByteString[0], length, NULL, NULL);
-
-    multiByteString.pop_back();
-    return multiByteString;
-}
-
-std::string GetAppdataDir() {
-    PWSTR localAppDataPath;
-    HRESULT result = SHGetKnownFolderPath(FOLDERID_LocalAppData, 0, NULL, &localAppDataPath);
-
-    if (!SUCCEEDED(result)) {
-        std::cerr << "Failed to retrieve Local AppData path." << std::endl;
-        return "";
-    }
-        
-    std::string localAppDataPathString = ConvertWideCharToMultiByte(localAppDataPath);
-    //std::cout << "Local AppData Path: " << localAppDataPathString << std::endl;
-
-    // Free the allocated memory for the path
-    CoTaskMemFree(localAppDataPath);
-
-    return localAppDataPathString;
-}
-
-
-bool IsStringInVector(const std::vector<std::string>& strings, const std::string& target) {
-    auto it = std::find(strings.begin(), strings.end(), target);
-    return (it != strings.end());
-}
-
-BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
-    WindowData* windowData = reinterpret_cast<WindowData*>(lParam);
-    if (IsWindowVisible(hwnd) && IsStringInVector(windowData->reqProcess, GetProcessNameFromHWND(hwnd))) {
-        windowData->windowsHandles.push_back(hwnd);
-    }
-    return TRUE;
-}
-
-std::vector<HWND> GetSortedWindowsHandles(const std::vector<std::string>& reqProcess) {
-    WindowData windowsData;
-    windowsData.reqProcess = reqProcess;
-
-    EnumWindows(EnumWindowsProc, reinterpret_cast<LPARAM>(&windowsData));
-
-    std::sort(windowsData.windowsHandles.begin(), windowsData.windowsHandles.end());
-    return windowsData.windowsHandles;
-}
-
-HWND GetNextWindowHandle(const std::vector<HWND>& windowsHandles, const HWND& window) {
-    auto it = std::find(windowsHandles.begin(), windowsHandles.end(), window);
-    if (it != windowsHandles.end() && std::next(it) != windowsHandles.end()) {
-        return *(std::next(it));
-    }
-    else {
-        return NULL; // Return NULL as an example when the conditions are not met
-    }
-}
-
-wchar_t* ConvertMultiByteToWideChar(const std::string& str)
-{
-    // Determine the required size of the buffer
-    int bufferSize = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, NULL, 0);
-
-    // Allocate a buffer to hold the wide character string
-    //std::vector<wchar_t> buffer(bufferSize);
-    wchar_t* buffer = new wchar_t[bufferSize];
-
-    // Convert the string to wide characters
-    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, buffer, bufferSize);
-
-    // Return a pointer to the converted string
-    return buffer;
-}
-
-HANDLE CreateNamedMutex(const std::string& mutexName, int& errorResult) {
-    wchar_t* convertedMutexName = ConvertMultiByteToWideChar(mutexName);
-    HANDLE mutexHandle = CreateMutexW(NULL, TRUE, convertedMutexName);
-    errorResult = GetLastError();
-    
-    delete convertedMutexName;
-    return mutexHandle;
-}
-
-bool IsControllerConnected(CXBOXController& controller) {
-    return controller.IsConnected();
-}
-
-bool IsControllerButtonPressed(CXBOXController& controller, const WORD& button) {
-    return (controller.GetState().Gamepad.wButtons & button);
-}
-
-void RefreshController(CXBOXController& controller) {
-    controller.RefreshState();
-}
-
-void SetControllerPlayerNumber(CXBOXController& controller, const int& playerNumber) {
-    controller.SetPlayerNumber(playerNumber);
-}
-
+std::vector<std::string> ReadReqProcessFile(const std::string& filePath);
+std::string GetProcessNameFromHWND(HWND hwnd);
+std::string ConvertWideCharToMultiByte(const wchar_t* wideCharString);
+std::string GetAppdataDir();
+bool IsStringInVector(const std::vector<std::string>& strings, const std::string& target);
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam);
+std::vector<HWND> GetSortedWindowsHandles(const std::vector<std::string>& reqProcess);
+HWND GetNextWindowHandle(const std::vector<HWND>& windowsHandles, const HWND& window);
+wchar_t* ConvertMultiByteToWideChar(const std::string& str);
+HANDLE CreateNamedMutex(const std::string& mutexName, int& errorResult);
+bool IsControllerConnected(CXBOXController& controller);
+bool IsControllerButtonPressed(CXBOXController& controller, const WORD& button);
+void RefreshController(CXBOXController& controller);
+void SetControllerPlayerNumber(CXBOXController& controller, const int& playerNumber);
 
 #endif
